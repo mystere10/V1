4eeac738a4e92f1f3316178be89aa5888799ed0f
Nkunzi Innocent <nkunziinnocent@gmail.com>
2018-11-17 11:10:51 +0200

    app.js modified

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 
-const usersRoutes = require('./usersRoutes');
+const usersRoutes = require('./routes');
 
 
 // Init app
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api/v1/users', usersRoutes);
+// app.use('/api/v1/users', usersRoutes);
 
 
 // PORT ASSIGNATION
